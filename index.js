932bd5d6ebd26de5b877e244f4f7759542956895
James <james@macbook-air.lan>
2022-06-20 21:00:36 -0700

    add questions

---
+++ index.js
@@ -0,0 +1,54 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your E-mail address?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "discription",
+    message: "What is the discription of your project?",
+  },
+  {
+    type: "input",
+    name: "contents",
+    message: "What is the contents of your project?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How to install your application?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Who is contributing in this project?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How to test your project?",
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
